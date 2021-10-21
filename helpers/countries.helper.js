2e048a200b5ec98c570b4cd8a1a6b50c57ae2d3c
Tatsiana A <astrouskaya@yahoo.com>
2021-10-21 11:31:38 -0400

    first commit

---
+++ helpers/countries.helper.js
@@ -0,0 +1,18 @@
+import supertest from 'supertest';
+
+class CountriesHelper{
+    constructor() {
+        this.response = null;
+    }
+
+    async get() {
+        await supertest(process.env.BASE_URL)
+            .get('/country_codes')
+            .then((res) => {
+                this.response = res
+            })
+        return this.response
+    }
+}
+
+export default CountriesHelper;
